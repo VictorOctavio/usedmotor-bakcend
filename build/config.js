adf6dcd33ecb37185965a9091e9ce714fac59671
Octavio <gaunavictoroctavio@gmail.com>
2023-01-01 09:51:37 -0300

    fixed-bug-port

--- build/config.js
+++ build/config.js
@@ -10,5 +10,5 @@ var _dotenv = require("dotenv");
 (0, _dotenv.config)();
 var MONGODB_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/failed';
 exports.MONGODB_URI = MONGODB_URI;
-var PORT = 8000 || 8080;
+var PORT = process.env.PORT || 8080;
 exports.PORT = PORT;
